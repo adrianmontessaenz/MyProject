364380ac2661b0423770c2a5ee97c17184e927aa
Adrian Montes <adrian.montes@digipen.edu>
2022-10-13 02:46:05 +0200

    Project Setup
    Now I will start with github issues

---
+++ Engine/Source/main.cpp
@@ -0,0 +1,13 @@
+/*// -----------------------------------------------------------------
+*  File:		main.cpp
+*  Brief:		Main of engine (temporal)
+*  Creation:	13/10/2022
+*  Last Update:	13/10/2022
+* 
+*  © 2022 Adrian Montes. All right reserved
+// -----------------------------------------------------------------*/
+
+int main(void)
+{
+	return 0;
+}
